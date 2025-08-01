185a1fbd431127a1663559e6463db4073fc6b039
Julius Kirsch <dev@frozenassassine.de>
2025-08-01 16:55:23 +0200

    fix: missing variables etc.

--- install.sh
+++ install.sh
@@ -1,255 +1,168 @@
 #!/bin/bash
 
-#Need some prep work
+# ============================
+# Constants
+# ============================
+CNT="[INFO]"
+COK="[OK]"
+CER="[ERROR]"
+CAT="[NOTICE]"
+INSTLOG="install.log"
+
+# ============================
+# Package Groups
+# ============================
 prep_stage=(
-    qt5-wayland 
-    qt5ct
-    qt6-wayland 
-    qt6ct
-    qt5-svg
-    qt5-quickcontrols2
-    qt5-graphicaleffects
-    gtk3 
-    polkit-gnome 
-    pipewire 
-    wireplumber 
-    jq 
-    wl-clipboard 
-    cliphist 
-    python-requests 
-    pacman-contrib
+    qt5-wayland qt5ct qt6-wayland qt6ct qt5-svg qt5-quickcontrols2 qt5-graphicaleffects
+    gtk3 polkit-gnome pipewire wireplumber jq wl-clipboard cliphist python-requests pacman-contrib
 )
 
-#software for nvidia GPU only
 nvidia_stage=(
-    linux-headers 
-    nvidia-dkms 
-    nvidia-settings 
-    libva 
-    libva-nvidia-driver-git
+    linux-headers nvidia-dkms nvidia-settings libva libva-nvidia-driver-git
 )
 
-#the main packages
 install_stage=(
-    kitty 
-    mako 
-    waybar
-    swww 
-    swaylock-effects 
-    swayidle
-    wlogout 
-    xdg-desktop-portal-hyprland 
-    swappy 
-    grim 
-    slurp 
-    thunar 
-    btop 
-    firefox
-    thunderbird 
-    mpv
-    pamixer 
-    pavucontrol 
-    brightnessctl 
-    bluez 
-    bluez-utils 
-    blueman 
-    network-manager-applet 
-    gvfs 
-    thunar-archive-plugin 
-    file-roller
-    papirus-icon-theme 
-    ttf-jetbrains-mono-nerd 
-    noto-fonts-emoji 
-    lxappearance 
-    xfce4-settings
-    nwg-look
-    sddm
-    rofi
-    rofi-emoji
-    tesseract
-    discord
-    visual-studio-code-bin
-    hyprpaper
-    blender
-    inkscape
-    ttf-fantasque-sans-mono
+    kitty mako waybar swww swaylock-effects swayidle wlogout
+    xdg-desktop-portal-hyprland swappy grim slurp thunar btop firefox thunderbird mpv
+    pamixer pavucontrol brightnessctl bluez bluez-utils blueman network-manager-applet
+    gvfs thunar-archive-plugin file-roller papirus-icon-theme ttf-jetbrains-mono-nerd
+    noto-fonts-emoji lxappearance xfce4-settings nwg-look sddm rofi rofi-emoji
+    tesseract discord visual-studio-code-bin hyprpaper blender inkscape ttf-fantasque-sans-mono
 )
 
-for str in ${myArray[@]}; do
-  echo $str
-done
-
-# function that would show a progress bar to the user
+# ============================
+# Functions
+# ============================
 show_progress() {
-    while ps | grep $1 &> /dev/null;
-    do
+    while ps -p "$1" &> /dev/null; do
         echo -n "."
         sleep 2
     done
-    echo -en "Done!\n"
-    sleep 2
+    echo -e "\nDone!"
+    sleep 1
 }
 
-# function that will test for a package and if not found it will attempt to install it
 install_software() {
-    # First lets see if the package is there
-    if yay -Q $1 &>> /dev/null ; then
+    if yay -Q "$1" &>> "$INSTLOG"; then
         echo -e "$COK - $1 is already installed."
     else
-        # no package found so installing
-        echo -en "$CNT - Now installing $1 ."
-        yay -S --noconfirm $1 &>> $INSTLOG &
+        echo -en "$CNT - Installing $1..."
+        yay -S --noconfirm "$1" &>> "$INSTLOG" &
         show_progress $!
-        # test to make sure package installed
-        if yay -Q $1 &>> /dev/null ; then
-            echo -e "\e[1A\e[K$COK - $1 was installed."
+        if yay -Q "$1" &>> "$INSTLOG"; then
+            echo -e "\e[1A\e[K$COK - $1 installed."
         else
-            # if this is hit then a package is missing, exit to review log
-            echo -e "\e[1A\e[K$CER - $1 install had failed, please check the install.log"
-            exit
+            echo -e "\e[1A\e[K$CER - Failed to install $1. Check $INSTLOG"
+            exit 1
         fi
     fi
 }
 
-# clear the screen
+# ============================
+# Start
+# ============================
+
 clear
+echo -e "$CNT - Starting Hyprland installation..."
 
-# set some expectations for the user
-echo -e "$CNT - You are about to execute a script that would attempt to setup Hyprland."
-sleep 1
-
-# let the user know that we will use sudo
-echo -e "$CNT - This script will run some commands that require sudo. You will be prompted to enter your password.
-If you are worried about entering your password then you may want to review the content of the script."
-sleep 1
-
-# give the user an option to exit out
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to continue with the install (y,n) ' CONTINST
-if [[ $CONTINST == "Y" || $CONTINST == "y" ]]; then
-    echo -e "$CNT - Setup starting..."
-    sudo touch /tmp/hyprinstall.tmp
-else
-    echo -e "$CNT - This script will now exit, no changes were made to your system."
+read -rep $'[\e[1;33mACTION\e[0m] - Continue? (y/n) ' CONTINST
+if [[ ! $CONTINST =~ ^[Yy]$ ]]; then
+    echo -e "$CNT - Exiting."
     exit
 fi
 
-# find the Nvidia GPU
+sudo touch /tmp/hyprinstall.tmp
+
+# Check for NVIDIA GPU
 if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
     ISNVIDIA=true
 else
     ISNVIDIA=false
 fi
 
-### Disable wifi powersave mode ###
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to disable WiFi powersave? (y,n) ' WIFI
-if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
+# Disable WiFi powersave
+read -rep $'[\e[1;33mACTION\e[0m] - Disable WiFi powersave? (y/n) ' WIFI
+if [[ $WIFI =~ ^[Yy]$ ]]; then
     LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
-    echo -e "$CNT - The following file has been created $LOC.\n"
-    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC &>> $INSTLOG
-    echo -en "$CNT - Restarting NetworkManager service, Please wait."
-    sleep 2
-    sudo systemctl restart NetworkManager &>> $INSTLOG
-    
-    #wait for services to restore (looking at you DNS)
-    for i in {1..6} 
-    do
-        echo -n "."
-        sleep 1
-    done
-    echo -en "Done!\n"
-    sleep 2
-    echo -e "\e[1A\e[K$COK - NetworkManager restart completed."
+    echo -e "$CNT - Creating $LOC"
+    echo -e "[connection]\nwifi.powersave = 2" | sudo tee "$LOC" &>> "$INSTLOG"
+    echo -en "$CNT - Restarting NetworkManager..."
+    sudo systemctl restart NetworkManager &>> "$INSTLOG"
+    sleep 6
+    echo -e "\e[1A\e[K$COK - NetworkManager restarted."
 fi
 
-#### Check for package manager ####
-if [ ! -f /sbin/yay ]; then  
-    echo -en "$CNT - Configuering yay."
-    git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
-    cd yay
-    makepkg -si --noconfirm &>> ../$INSTLOG &
-    show_progress $!
-    if [ -f /sbin/yay ]; then
-        echo -e "\e[1A\e[K$COK - yay configured"
-        cd ..
-        
-        # update the yay database
-        echo -en "$CNT - Updating yay."
-        yay -Suy --noconfirm &>> $INSTLOG &
-        show_progress $!
-        echo -e "\e[1A\e[K$COK - yay updated."
+# yay install
+if ! command -v yay &> /dev/null; then
+    echo -en "$CNT - Installing yay..."
+    git clone https://aur.archlinux.org/yay.git &>> "$INSTLOG"
+    pushd yay
+    makepkg -si --noconfirm &>> ../"$INSTLOG"
+    popd
+    if ! command -v yay &> /dev/null; then
+        echo -e "\e[1A\e[K$CER - yay install failed."
+        exit 1
     else
-        # if this is hit then a package is missing, exit to review log
-        echo -e "\e[1A\e[K$CER - yay install failed, please check the install.log"
-        exit
+        echo -e "\e[1A\e[K$COK - yay installed."
+        yay -Suy --noconfirm &>> "$INSTLOG"
     fi
 fi
 
+# Install packages
+read -rep $'[\e[1;33mACTION\e[0m] - Install packages now? (y/n) ' INST
+if [[ $INST =~ ^[Yy]$ ]]; then
 
-
-### Install all of the above packages ####
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install the packages? (y,n) ' INST
-if [[ $INST == "Y" || $INST == "y" ]]; then
-
-    # Prep Stage - Bunch of needed items
-    echo -e "$CNT - Prep Stage - Installing needed components, this may take a while..."
-    for SOFTWR in ${prep_stage[@]}; do
-        install_software $SOFTWR 
+    echo -e "$CNT - Installing preparation packages..."
+    for pkg in "${prep_stage[@]}"; do
+        install_software "$pkg"
     done
 
-    # Setup Nvidia if it was found
     if [[ "$ISNVIDIA" == true ]]; then
-        echo -e "$CNT - Nvidia GPU support setup stage, this may take a while..."
-        for SOFTWR in ${nvidia_stage[@]}; do
-            install_software $SOFTWR
+        echo -e "$CNT - Installing NVIDIA packages..."
+        for pkg in "${nvidia_stage[@]}"; do
+            install_software "$pkg"
         done
-    
-        # update config
-        sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+
+        sudo sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
         sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-        echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+        echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia.conf &>> "$INSTLOG"
     fi
 
-    # Install the correct hyprland version
-    echo -e "$CNT - Installing Hyprland, this may take a while..."   
+    echo -e "$CNT - Installing Hyprland..."
     install_software hyprland
 
-    # Stage 1 - main components
-    echo -e "$CNT - Installing main components, this may take a while..."
-    for SOFTWR in ${install_stage[@]}; do
-        install_software $SOFTWR 
+    echo -e "$CNT - Installing main software..."
+    for pkg in "${install_stage[@]}"; do
+        install_software "$pkg"
     done
 
-    # Start the bluetooth service
-    echo -e "$CNT - Starting the Bluetooth Service..."
-    sudo systemctl enable --now bluetooth.service &>> $INSTLOG
-    sleep 2
+    echo -e "$CNT - Enabling Bluetooth service..."
+    sudo systemctl enable --now bluetooth.service &>> "$INSTLOG"
 
-    # Enable the sddm login manager service
-    echo -e "$CNT - Enabling the SDDM Service..."
-    sudo systemctl enable sddm &>> $INSTLOG
-    sleep 2
-    
-    # Clean out other portals
-    echo -e "$CNT - Cleaning out conflicting xdg portals..."
-    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
+    echo -e "$CNT - Enabling SDDM login manager..."
+    sudo systemctl enable sddm &>> "$INSTLOG"
+
+    echo -e "$CNT - Removing conflicting portals..."
+    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> "$INSTLOG"
 fi
 
+# Copy config
+echo -e "$CNT - Copying config files..."
+cp -r .config/* ~/.config/
+echo "$COK - Copied config to ~/.config"
 
-#copy config files:
-cp -r "./.config"* "~/.config"
-echo "Copied config files to ~/.config"
+# Done
+echo -e "$COK - Installation complete."
 
-### Script is done ###
-echo -e "$CNT - Script has completed!"
 if [[ "$ISNVIDIA" == true ]]; then 
-    echo -e "$CAT - Since we attempted to setup an Nvidia GPU the script will now end and you should reboot.
-    Please type 'reboot' at the prompt and hit Enter when ready."
+    echo -e "$CAT - Reboot now to apply NVIDIA changes. Type 'reboot' to proceed."
     exit
 fi
 
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to start Hyprland now? (y,n) ' HYP
-if [[ $HYP == "Y" || $HYP == "y" ]]; then
-    exec sudo systemctl start sddm &>> $INSTLOG
+read -rep $'[\e[1;33mACTION\e[0m] - Start Hyprland via SDDM now? (y/n) ' HYP
+if [[ $HYP =~ ^[Yy]$ ]]; then
+    sudo systemctl start sddm &>> "$INSTLOG"
 else
+    echo -e "$CNT - You can start Hyprland later with: sudo systemctl start sddm"
     exit
 fi
